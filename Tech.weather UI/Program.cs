2945bf58cf507e985617e882c3d7ded1df39f2a0
connitech2020 <yosef.tagawi1@gmail.com>
2022-04-26 23:04:23 +0300

    commit

--- Tech.weather UI/Program.cs
+++ Tech.weather UI/Program.cs
@@ -8,7 +8,7 @@ namespace Tech.weather_UI
 {
     static class Program
     {
-        /// <summary>
+        ///// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
